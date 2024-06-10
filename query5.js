5544803ff2bd42f0c373f490e3a0ee3b39c35232
Beining Zhu <bxz@umich.edu>
2024-06-10 02:52:43 -0400

    a

--- query5.js
+++ query5.js
@@ -39,11 +39,10 @@ function oldest_friend(dbname) {
 
     // Combine the results
     let combined = original.concat(inverse);
-    printjson(combined);
 
     // Insert combined results into the final collection
     db.flat_friends_combined.insertMany(combined);
-
+    db.flat_friends_combined.find();
     // Clean up temporary collections
     db.flat_friends_temp.drop();
     db.flat_friends_inverse.drop();
